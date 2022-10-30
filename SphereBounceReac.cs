51833b3463a543994c518045491ad52045155471
Omar4GH <103641847+Omar4GH@users.noreply.github.com>
2022-10-31 01:32:52 +0300

    Add files via upload

---
+++ SphereBounceReac.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBounceReac : MonoBehaviour
+{
+
+ void OnCollisionEnter(Collision other) {  
+        Debug.Log("HIT");  
+    }  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
